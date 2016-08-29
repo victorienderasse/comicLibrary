8ea50ca0bcd8b435766858cc8f715796e9ea22d7
Victorien <victorien.derasse@hotmail.com>
2016-08-29 11:52:07 +0200

    innerhtml

--- public/javascripts/library.js
+++ public/javascripts/library.js
@@ -1,4 +1,5 @@
 /**
  * Created by Victorien on 29-08-16.
  */
-var myDiv = document.getElementById('lastComic').innerHTML('<p>Bonjour</p>');
+var myDiv = document.getElementById('lastComic');
+myDiv.innerHTML('<p>Bonjour</p>');
