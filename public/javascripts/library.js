91056b6cad3d0c38f820f091a29bc1bbd84af318
Victorien <victorien.derasse@hotmail.com>
2016-08-29 15:04:36 +0200

    index

--- public/javascripts/library.js
+++ public/javascripts/library.js
@@ -1,4 +1,35 @@
 /**
  * Created by Victorien on 29-08-16.
  */
+//Connection BDD
+const mysql = require('mysql')
 
+const connection = mysql.createConnection({
+    host : '192.168.1.100',
+    user : 'root',
+    password : '221193m',
+    database : 'BD'
+});
+
+//Events-------------------------------------
+
+//Add last added to table
+
+
+//action for addComic btn
+document.getElementById('add-comic-btn').addEventListener('click', function () {
+    
+});
+
+//get form to add comic
+document.getElementById('add-comic-form-btn').addEventListener('click', function () {
+
+});
+
+//action for show comic btn
+document.getElementById('show-comic-btn').addEventListener('click', function () {
+    
+});
+
+
+//Functions------------------------------------
