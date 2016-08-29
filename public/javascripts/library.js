73182981bb7d8b5709efaaadeca62908a4a3e652
Victorien <victorien.derasse@hotmail.com>
2016-08-29 16:23:49 +0200

    index

--- public/javascripts/library.js
+++ public/javascripts/library.js
@@ -27,11 +27,11 @@ document.getElementById('add-comic-form-btn').addEventListener('click', function
 
 
 //Functions------------------------------------
-    
+
 
 
 function addComic() {
-    console.log('addComic - function');
+    console.log('addComic - function ');
     var form = document.getElementById('add-comic-form');
     socket.emit('addComic',{collection: form.collection.value, title: form.title.value, number: form.number.value, publishingHouse: form.publishingHouse.value, authors: form.authors.value});
 }
