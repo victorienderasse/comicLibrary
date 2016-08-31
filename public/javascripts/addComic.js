02a2818981569d4677f7321e40363ab31c9bfe46
Victorien <victorien.derasse@hotmail.com>
2016-08-31 16:26:54 +0200

    showComic

--- public/javascripts/addComic.js
+++ public/javascripts/addComic.js
@@ -5,9 +5,7 @@
 
 //get form to add comic
 document.getElementById('add-comic-form-btn').addEventListener('click', function () {
-    console.log('addComic - event listener');
     addComic();
-    window.location = 'http://192.168.1.100:8888';
 });
 
 
@@ -21,9 +19,8 @@ document.getElementById('backBtn').addEventListener('click', function(){
 
 
 function addComic() {
-    console.log('addComic - function ');
-    //check
+    console.log('addComic');
     var form = document.getElementById('add-comic-form');
-
     socket.emit('addComic',{collection: form.collection.value, title: form.title.value, number: form.number.value, publishingHouse: form.publishingHouse.value, authors: form.authors.value});
+    window.location = 'http://192.168.1.100:8888/addComic';
 }
